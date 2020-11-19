98957b79b54e66a0c91c1665a9345b57df859e0c
Munderstand <asukiller101@gmail.com>
2020-11-19 19:25:48 +0100

    Creation

---
+++ robocopy-command/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robocopy_command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
